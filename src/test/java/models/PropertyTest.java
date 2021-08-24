3bf9c003ef05f5f493ce09efc2bc46f4a4c2e9ab
Steve-njeri <stevenjeri254@gmail.com>
2021-08-24 14:59:28 +0300

    added get methods and run tests to check whether variables are returned correctly

--- src/test/java/models/PropertyTest.java
+++ src/test/java/models/PropertyTest.java
@@ -11,8 +11,32 @@ class PropertyTest {
         assertEquals("Atlantic Edge", property.getName());
     }
 
-//    helper
+    @Test
+    void getTypeReturnsCorrectPropertyType() {
+        Property property = setupProperty();
+        assertEquals("Apartment", property.getType());
+    }
+
+    @Test
+    void getLocationReturnsCorrectLocation() {
+        Property property = setupProperty();
+        assertEquals("Karen", property.getLocation());
+    }
+
+    @Test
+    void getDescriptionReturnsCorrectDescription() {
+        Property property = setupProperty();
+        assertEquals("five bedroom", property.getDescription());
+    }
+
+    @Test
+    void getRentReturnsCorrectRent() {
+        Property property = setupProperty();
+        assertEquals(30000, property.getRent());
+    }
+
+    //helper
     private Property setupProperty() {
-        return new Property("Atlantic Edge", "Apartment", "Lavinghton", "five bedroom", 30000 );
+        return new Property("Atlantic Edge", "Apartment", "Karen", "five bedroom", 30000 );
     }
 }
