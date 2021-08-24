a8862c3361cb5f3bb31581dfb96c808ae177d07c
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-08-24 18:15:15 +0300

    created the data access object for tenant with all the necessary methods and tests,while doing this i updated the buld.gradle

--- src/main/java/dao/TenantDao.java
+++ src/main/java/dao/TenantDao.java
@@ -1,5 +1,23 @@
 package dao;
 
+import models.Tenant;
+
+import java.util.List;
+
 public interface TenantDao {
 
+    //create tenant
+    void addTenant(Tenant tenant);
+
+    //read tenants from database
+    List<Tenant> getAllTenants();
+    Tenant findTenantById(int id);
+    //List<Tenant> getAllTenantsByPropertyId(int propertyId);
+
+    //Update
+    void updateTenantInfo(int id,String name,String phoneNumber,int propertyId);
+
+    //delete tenant from database
+    void deleteTenantById(int id);
+    void clearAll();
 }
--- src/test/java/dao/Sql2oTenantDaoTest.java
+++ src/test/java/dao/Sql2oTenantDaoTest.java
@@ -1,17 +1,96 @@
 package dao;
 
+import models.Tenant;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Sql2o;
+
+import org.sql2o.Connection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Sql2oTenantDaoTest {
+    private Connection conn;
+    private Sql2oTenantDao tenantDao;
 
     @BeforeEach
     void setUp() {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "issah", "issah9960");
+        tenantDao= new Sql2oTenantDao(sql2o);
+        conn = sql2o.open();
     }
 
     @AfterEach
     void tearDown() {
+        conn.close();
+        System.out.println("connection closed");
     }
+
+    @Test
+    void addingTenantSetsId() {
+        Tenant testTenant = setUpTenant();
+        int tenantId=testTenant.getId();
+        tenantDao.addTenant(testTenant);
+        assertNotEquals(tenantId,tenantDao.getAllTenants().size());
+    }
+
+    @Test
+    void getAllTenants() {
+        Tenant testTenant = setUpTenant();
+        tenantDao.addTenant(testTenant);
+        assertEquals(1,tenantDao.getAllTenants().size());
+    }
+
+    @Test
+    void findTenantById() {
+        Tenant testTenant = setUpTenant();
+        tenantDao.addTenant(testTenant);
+        Tenant otherTenant = setUpTenant();
+        tenantDao.addTenant(otherTenant);
+
+        assertEquals(otherTenant,tenantDao.findTenantById(otherTenant.getId()));
+    }
+
+    @Test
+    void deleteTenantById() {
+        Tenant testTenant = setUpTenant();
+        tenantDao.addTenant(testTenant);
+        Tenant otherTenant = setUpTenant();
+        tenantDao.addTenant(otherTenant);
+
+        tenantDao.deleteTenantById(testTenant.getId());
+
+        assertEquals(1,tenantDao.getAllTenants().size());
+    }
+
+    @Test
+    void updateTenantInfo() {
+        Tenant testTenant = setUpTenant();
+        tenantDao.addTenant(testTenant);
+        tenantDao.updateTenantInfo(testTenant.getId(),"issah","54565528",1547896);
+        Tenant updatedTenant = tenantDao.findTenantById(testTenant.getId());
+        assertEquals("54565528",updatedTenant.getPhoneNumber());
+    }
+
+    @Test
+    void clearAll() {
+        Tenant testTenant = setUpTenant();
+        tenantDao.addTenant(testTenant);
+        Tenant otherTenant = setUpTenant();
+        tenantDao.addTenant(otherTenant);
+
+        tenantDao.clearAll();
+        assertEquals(0,tenantDao.getAllTenants().size());
+    }
+
+
+
+    //helpers
+    public Tenant setUpTenant(){
+        return new Tenant("issah","54789654",698574);
+    };
+
+
 }
