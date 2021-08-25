e0b216d6b62a28e2ede68a6f304978747254d699
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-08-25 14:02:36 +0300

    added tenant routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,9 @@
 import com.google.gson.Gson;
 import dao.Sql2oPropertyDao;
+import dao.Sql2oTenantDao;
 import exceptions.ApiException;
 import models.Property;
+import models.Tenant;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
@@ -14,6 +16,7 @@ public class App {
     public static void main(String[] args) {
 
         Sql2oPropertyDao propertyDao;
+        Sql2oTenantDao tenantDao;
         Connection conn;
         Gson gson = new Gson();
 
@@ -22,6 +25,7 @@ public class App {
         Sql2o sql2o = new Sql2o(connectionString, "", "");
 
         propertyDao = new Sql2oPropertyDao(sql2o);
+        tenantDao = new Sql2oTenantDao(sql2o);
 
 
         //READ
@@ -35,6 +39,17 @@ public class App {
             }
         });
 
+        //READ tenants
+        get("/tenants", "application/json", (req, res) -> {
+            System.out.println(tenantDao.getAllTenants());
+
+            if(tenantDao.getAllTenants().size() > 0){
+                return gson.toJson(tenantDao.getAllTenants());
+            } else {
+                return "{\"message\":\"I'm sorry, but no tenants are currently listed in the database.\"}";
+            }
+        });
+
         // Get property by id
         get("/properties/:id", "application/json", (req, res) -> {
             int restaurantId = Integer.parseInt(req.params("id"));
@@ -45,12 +60,30 @@ public class App {
             return gson.toJson(propertyDao);
         });
 
+        // Get tenant by id
+        get("/tenants/:id", "application/json", (req, res) -> {
+            int tenantId = Integer.parseInt(req.params("id"));
+            Tenant tenantToFind = tenantDao.findTenantById(tenantId);
+            if (tenantDao == null){
+                throw new ApiException(404, String.format("No tenant with the id: \"%s\" exists", req.params("id")));
+            }
+            return gson.toJson(tenantDao);
+        });
+
         //CREATE
         post("/properties/new", "application/json", (req, res) -> {
             Property property = gson.fromJson(req.body(), Property.class);
             propertyDao.add(property);
             res.status(201);
             return gson.toJson(property);
+        });
+
+        //CREATE
+        post("/tenants/new", "application/json", (req, res) -> {
+            Tenant tenant = gson.fromJson(req.body(), Tenant.class);
+            tenantDao.addTenant(tenant);
+            res.status(201);
+            return gson.toJson(tenant);
         });
 
         //FILTERS
