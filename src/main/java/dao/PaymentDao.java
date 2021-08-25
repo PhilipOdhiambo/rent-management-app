63933ecc477bff812b65485ee3027c9bbeb2296f
Philip Odhiambo <jofonowino@gmail.com>
2021-08-25 08:26:36 +0300

    added payment module

--- src/main/java/dao/PaymentDao.java
+++ src/main/java/dao/PaymentDao.java
@@ -1,4 +1,22 @@
 package dao;
 
+import models.Payment;
+
+import java.util.List;
+
 public interface PaymentDao {
+    //create
+    public void add(Payment payment);
+
+    // read
+    public List<Payment> getAll();
+    public Payment getById (int id);
+
+
+    // update
+    public void update(Payment updatedPayment);
+
+    //delete
+    public void deleteById(int id);
+    public void deleteAll();
 }
